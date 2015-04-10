b510ffff20f5b0b4786ed872022af721e610dc52
Sky <812935999@qq.com>
2015-04-10 11:32:13 +0800

    The first Test

--- src/com/Sky/BalloonGame4/BalloonGame.java
+++ src/com/Sky/BalloonGame4/BalloonGame.java
@@ -3,7 +3,7 @@ package com.Sky.BalloonGame4;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MyActivity extends Activity {
+public class BalloonGame extends Activity {
     /**
      * Called when the activity is first created.
      */
